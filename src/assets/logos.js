6add615a5806630931f9b6f59122e8b746acfe85
hoang bac <hoangvietbac090699@gmail.com>
2023-06-27 01:19:00 +0700

    :sparkles: Add layout

--- src/assets/logos.js
+++ src/assets/logos.js
@@ -2,6 +2,7 @@ const logos = {
   logoPage: require('./logos/logo.svg').default,
   discord: require('./logos/svgexport-3.svg').default,
   github: require('./logos/github.svg').default,
+  npm: require('./logos/npm.svg').default,
   logoFooter: require('./logos/logofooter.png'),
   risingStars: require('./logos/rising-stars.png')
 };
